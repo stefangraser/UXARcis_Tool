ce68da14c3bfb57b911c98d53adc856eb332586e
Stefan Graser <98594452+stefangraser@users.noreply.github.com>
2025-04-11 13:05:36 +0200

    g

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ st.title("UXARcis-Evaluationstool")
 st.markdown("Effektive UX-Analyse für AR-Autoren.")
 
 
+
 # Verbindung zu Neon (robust, mit eigenem Engine)
 try:
     username = st.secrets["connections"]["neon"]["username"]
